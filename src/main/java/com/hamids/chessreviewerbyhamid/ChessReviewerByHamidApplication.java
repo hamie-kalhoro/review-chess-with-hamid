24bb7c21b3c55cd644d2bdbedc4ae9fe89a43a7e
Hamid Ali Khan <aleehamid1024@gmail.com>
2025-12-25 12:52:39 +0500

    Initial commit

---
+++ src/main/java/com/hamids/chessreviewerbyhamid/ChessReviewerByHamidApplication.java
@@ -0,0 +1,13 @@
+package com.hamids.chessreviewerbyhamid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessReviewerByHamidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessReviewerByHamidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamids/chessreviewerbyhamid/ChessReviewerByHamidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamids.chessreviewerbyhamid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessReviewerByHamidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
